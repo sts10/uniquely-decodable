11e083ed4016af8f4fc52d27d9752e8ea3d1939a
Sam Schlinkert <sschlinkert@gmail.com>
2022-06-30 19:31:21 -0400

    attempts a recursive approach

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use array_tool::vec::Uniq;
+
 fn main() {
     let alphabet1: Vec<String> = vec!["0".to_string(), "10".to_string(), "110".to_string()]; // uniquely decodable
     let alphabet2: Vec<String> = vec!["1".to_string(), "11".to_string(), "10".to_string()]; // 1110 can be 11|10 OR 1|1|10; so NOT uniquely decodable
@@ -10,18 +12,73 @@ fn main() {
         "spills".to_string(),
         "unmoved".to_string(),
     ];
+    let alphabet6: Vec<String> = vec![
+        "0".to_string(),
+        "10".to_string(),
+        "110".to_string(),
+        "111".to_string(),
+    ]; // UDC
+    let alphabet7: Vec<String> = vec!["0".to_string(), "1".to_string(), "01".to_string()]; // No
+    let alphabet8: Vec<String> = vec!["10".to_string(), "11".to_string(), "111".to_string()]; // No
 
-    // for word in alphabet1 {
-    //     println!("{}", word);
-    // }
-    assert!(naive_uniquely_decodable(alphabet1));
-    assert!(!naive_uniquely_decodable(alphabet2));
-    assert!(naive_uniquely_decodable(alphabet3));
-    assert!(!naive_uniquely_decodable(alphabet4));
-    assert!(!naive_uniquely_decodable(alphabet5));
+    // assert!(naive_uniquely_decodable(alphabet1));
+    // assert!(!naive_uniquely_decodable(alphabet2));
+    // assert!(naive_uniquely_decodable(alphabet3));
+    // assert!(!naive_uniquely_decodable(alphabet4));
+    // assert!(!naive_uniquely_decodable(alphabet5));
+
+    assert!(recursive_uniquely_decodable(alphabet1));
+    assert!(!recursive_uniquely_decodable(alphabet2));
+    assert!(recursive_uniquely_decodable(alphabet3));
+    assert!(!recursive_uniquely_decodable(alphabet4));
+    // assert!(!recursive_uniquely_decodable(alphabet5));
+    assert!(recursive_uniquely_decodable(alphabet6));
+    assert!(!recursive_uniquely_decodable(alphabet7));
+    assert!(!recursive_uniquely_decodable(alphabet8));
 }
 
-fn naive_uniquely_decodable(list: Vec<String>) -> bool {
+fn add_dangling_suffixes_to_a_list(list: &[String]) -> Vec<String> {
+    let mut list_plus_suffixes: Vec<String> = list.to_vec();
+    for i in 0..list.len() {
+        for j in 0..list.len() {
+            match get_dangling_suffix_if_exists(&list[i], &list[j]) {
+                Some(dangling_suffix) => list_plus_suffixes.push(dangling_suffix),
+                None => continue,
+            }
+        }
+    }
+    list_plus_suffixes
+}
+
+fn recursive_uniquely_decodable(list: Vec<String>) -> bool {
+    let new_list = add_dangling_suffixes_to_a_list(&list);
+    println!("list is now {:?}", new_list);
+    if new_list.len() == list.len() {
+        println!("Same length?!");
+        return true;
+    }
+
+    println!("Checking uniqueness");
+    // if new_list.is_unique() {
+    if vec_is_unique(&new_list) {
+        // real hazy here
+        let result = recursive_uniquely_decodable(new_list);
+        return result;
+    } else {
+        println!("Not unique! Raturning false!");
+        return false;
+    }
+    true
+}
+
+fn vec_is_unique(list: &[String]) -> bool {
+    let mut unique_list = list.to_vec();
+    unique_list.sort();
+    unique_list.dedup();
+    list.len() == unique_list.len()
+}
+
+fn _naive_uniquely_decodable(list: Vec<String>) -> bool {
     let mut list_plus_suffixes: Vec<String> = list.clone();
     // for potential_prefix_word in &list {
     for i in 0..list_plus_suffixes.len() {
