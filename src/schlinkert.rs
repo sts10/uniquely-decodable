984c8149fb789fee48e81b3fa3e5df27cc3a4421
Sam Schlinkert <sschlinkert@gmail.com>
2024-01-19 13:08:52 -0500

    attempts to use DashSet to hopefully speed thipngs up, but I can't get it to compile rn

--- src/schlinkert.rs
+++ src/schlinkert.rs
@@ -5,7 +5,7 @@
 //! I followed
 //! <https://github.com/danhales/blog-sardinas-patterson/blob/master/index.ipynb>
 //! very closely. Since then, other contributors have refactored it.
-use std::collections::HashSet;
+use dashmap::DashSet;
 
 /// Return true if the list is uniquely decodable, false if not. I
 /// don't _think_ we need to check reversed words in this case.
@@ -14,20 +14,20 @@ pub fn is_uniquely_decodable<T: AsRef<str>>(c: &[T]) -> bool {
 }
 
 /// Generate c for any number n
-fn generate_cn<'a>(c: &HashSet<&'a str>, cn_minus_1: &HashSet<&'a str>) -> HashSet<&'a str> {
-    let mut cn = HashSet::new();
+fn generate_cn<'a>(c: &DashSet<&'a str>, cn_minus_1: &DashSet<&'a str>) -> DashSet<&'a str> {
+    let mut cn = DashSet::new();
 
     for w1 in c.iter() {
         for w2 in cn_minus_1.iter() {
             if w1.len() > w2.len() && w1.starts_with(w2) {
                 // w2 is a prefix word of w1
-                // so, we're going to add the dangling suffix to a new HashSet
+                // so, we're going to add the dangling suffix to a new DashSet
                 // called cn
                 cn.insert(&w1[w2.len()..]);
             }
             if w2.len() > w1.len() && w2.starts_with(w1) {
                 // w1 is a prefix word of w2
-                // so, we're going to add the dangling suffix to a new HashSet
+                // so, we're going to add the dangling suffix to a new DashSet
                 // called cn
                 cn.insert(&w2[w1.len()..]);
             }
@@ -36,7 +36,7 @@ fn generate_cn<'a>(c: &HashSet<&'a str>, cn_minus_1: &HashSet<&'a str>) -> HashS
     cn
 }
 
-fn generate_c_infinity_with_a_halt_break<'a>(c: &'a HashSet<&str>) -> HashSet<&'a str> {
+fn generate_c_infinity_with_a_halt_break<'a>(c: &'a DashSet<&str>) -> DashSet<&'a str> {
     let mut cn = generate_cn(c, c);
     let mut cs = cn.clone();
 
@@ -54,7 +54,7 @@ fn generate_c_infinity_with_a_halt_break<'a>(c: &'a HashSet<&str>) -> HashSet<&'
 }
 
 /// Returns true if c is uniquely decodable
-fn sardinas_patterson_theorem(c: HashSet<&str>) -> bool {
+fn sardinas_patterson_theorem(c: DashSet<&str>) -> bool {
     let c_infinity = generate_c_infinity_with_a_halt_break(&c);
     c.is_disjoint(&c_infinity)
 }
